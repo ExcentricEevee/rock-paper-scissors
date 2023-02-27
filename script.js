038c8c678dcd1b768381284e7b337a6db27a99fb
Curtis <excentriceevee@hotmail.com>
2023-02-27 18:29:52 -0500

    Add logic for checking a round's win state
    playRound has a rather ugly looking conditional check for if the player
    wins, loses, or draws. I don't think a switch case applies here.
    Perhaps better text formatting will work, or give it its own function.
    The purpose of playRound is to be used as many times as desired by
    the game() function and keep score for each round played.

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+function game () {
+    let computerSelection;
+    for (let i = 0; i < 5; i++) {
+        computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection))
+    }
+}
+
+const playerSelection = "rock";
+
+function playRound(playerSelection, computerSelection) {
+    //make it case-insensitive
+    playerSelection = playerSelection.toLowerCase();
+
+    // this doesn't really look pretty
+    if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper") {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+    } 
+    else if (playerSelection === "rock" && computerSelection === "paper" ||
+                 playerSelection === "paper" && computerSelection === "scissors" ||
+                playerSelection === "scissors" && computerSelection === "rock") {
+            return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return `It's a draw! It was ${playerSelection} and ${computerSelection}`;
+    }
+}
+
 function getComputerChoice() {
     const randomNumber = getRandomInteger(0, 2);
 
     let gameChoice;
-    switch(randomNumber) {
+    switch (randomNumber) {
         case 0:
             gameChoice = "rock";
             break;
