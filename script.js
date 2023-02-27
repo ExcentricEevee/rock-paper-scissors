e403de7b29479c9efcdbc403fde38eea1daae4f0
Curtis <excentriceevee@hotmail.com>
2023-02-27 16:57:36 -0500

    Make an RNG system for making CPU move

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    const cpuChoice = "rock";
+    const randomNumber = getRandomInteger(0, 2);
+
+    console.log(randomNumber);
+}
+
+// min and max values are both included
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
