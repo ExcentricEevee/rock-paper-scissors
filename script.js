27365c223328fcd4fdfe97e9a5bccc5cf40e8c4d
Curtis <excentriceevee@hotmail.com>
2023-03-10 00:41:24 -0500

    Make game end once a player reaches 5 points
    Comes with some code cleanup in order to make message displays in div
    elements more clean and presentable, while also keeping the code itself
    more orderly with slightly more modular methods. Needs logic for resetting
    score, and could use some refactoring. CSS styling optional.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
 
-div = document.querySelector('div.results');
+resultsDiv = document.querySelector('div.results');
+scoreDiv = document.querySelector('div.score');
+winnerDiv = document.querySelector('div.winner');
+
 buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -13,22 +16,54 @@ function playRound(playerSelection, computerSelection) {
     // make it case-insensitive
     playerSelection = playerSelection.toLowerCase();
 
-    // refresh display at new round
-    div.textContent = "";
-    div.textContent += `You chose ${playerSelection}, and I chose ${computerSelection}...`;
+    refreshDisplay();
+    resultsDiv.textContent = `You chose ${playerSelection}, and I chose ${computerSelection}...`;
     if (playerSelection === "rock" && computerSelection === "scissors" ||
         playerSelection === "paper" && computerSelection === "rock" ||
         playerSelection === "scissors" && computerSelection === "paper") {
-            playerScore++;
-            div.textContent += `so you win! The score is now ${playerScore} to ${computerScore}`;
+            updateScore("player");
+            resultsDiv.textContent += `so you win!`;
+            if (playerScore === 5) endGame("player");
     } else if (playerSelection === "rock" && computerSelection === "paper" ||
                  playerSelection === "paper" && computerSelection === "scissors" ||
                 playerSelection === "scissors" && computerSelection === "rock") {
-                    computerScore++;
-                    div.textContent += `so I win! The score is now ${playerScore} to ${computerScore}`;
+                    updateScore("CPU");
+                    resultsDiv.textContent += `so I win!`;
+                    if (computerScore === 5) endGame("CPU");
     } else {
-        div.textContent += `it's a draw! The score is still ${playerScore} to ${computerScore}`;
+        resultsDiv.textContent += `it's a draw!`;
     }
+}
+
+function refreshDisplay() {
+    resultsDiv.textContent = "";
+    scoreDiv.textContent = `You: ${playerScore} | Me: ${computerScore}`;
+    winnerDiv.textContent = "";
+}
+
+function updateScore(winCase) {
+    switch (winCase) {
+        case "player":
+            playerScore++;
+            break;
+        case "CPU":
+            computerScore++;
+            break;
+    }
+
+    scoreDiv.textContent = `You: ${playerScore} | Me: ${computerScore}`;
+}
+
+function endGame(winCase) {
+    switch (winCase) {
+        case "player":
+            winnerDiv.textContent = "You win the game! Thanks for playing!";
+            break;
+        case "CPU":
+            winnerDiv.textContent = "I win the game! Wanna go another round?";
+            break;
+    }
+
 }
 
 function checkForWin() {
