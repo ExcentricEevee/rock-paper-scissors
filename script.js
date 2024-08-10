f3cc6e088267fac940d33667efcecaea368c8ddf
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2024-08-10 17:34:47 -0400

    Create function for playing a whole game
    The score variables and playRound() were repositioned to be inside of
    playGame() in order to encapsulate their information.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     const choice = Math.floor(Math.random() * 3);
     switch (choice) {
@@ -17,21 +14,41 @@ function getHumanChoice() {
     return prompt('Rock, Paper, or Scissors?');
 }
 
-function playRound(humanChoice, computerChoice) {
-    // make humanChoice case-insensitive
-    humanChoice = humanChoice.toLowerCase();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-    if (humanChoice === 'rock' && computerChoice === 'scissors' ||
-        humanChoice === 'paper' && computerChoice === 'rock' ||
-        humanChoice === 'scissors' && computerChoice === 'paper') {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-        humanScore += 1;
-    } else if (computerChoice === 'rock' && humanChoice === 'scissors' ||
-        computerChoice === 'paper' && humanChoice === 'rock' ||
-        computerChoice === 'scissors' && humanChoice === 'paper') {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-        computerScore += 1;
+    for(let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You won the whole game!");
+    } else if (computerScore < humanScore) {
+        console.log('The computer wins this time!')
     } else {
-        console.log('It\'s a tie!');
+        console.log('It ends in a tie!')
+    }
+    console.log(`Your score: ${humanScore} | CPU score: ${computerScore}`)
+
+    function playRound(humanChoice, computerChoice) {
+        // make humanChoice case-insensitive
+        humanChoice = humanChoice.toLowerCase();
+    
+        if (humanChoice === 'rock' && computerChoice === 'scissors' ||
+            humanChoice === 'paper' && computerChoice === 'rock' ||
+            humanChoice === 'scissors' && computerChoice === 'paper') {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+            humanScore++;
+        } else if (computerChoice === 'rock' && humanChoice === 'scissors' ||
+            computerChoice === 'paper' && humanChoice === 'rock' ||
+            computerChoice === 'scissors' && humanChoice === 'paper') {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        } else {
+            console.log(`It\'s a tie! It was ${humanChoice} and ${computerChoice}`);
+        }
     }
 }
