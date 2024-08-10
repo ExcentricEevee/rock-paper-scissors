43a267000238bd826209c83b6e0b3ec1f5148a84
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2024-08-10 16:55:39 -0400

    Create function for playing a single round

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function getComputerChoice() {
 function getHumanChoice() {
     return prompt('Rock, Paper, or Scissors?');
 }
+
+function playRound(humanChoice, computerChoice) {
+    // make humanChoice case-insensitive
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === 'rock' && computerChoice === 'scissors' ||
+        humanChoice === 'paper' && computerChoice === 'rock' ||
+        humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore += 1;
+    } else if (computerChoice === 'rock' && humanChoice === 'scissors' ||
+        computerChoice === 'paper' && humanChoice === 'rock' ||
+        computerChoice === 'scissors' && humanChoice === 'paper') {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        computerScore += 1;
+    } else {
+        console.log('It\'s a tie!');
+    }
+}
